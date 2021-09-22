484ca99f7d3e341d5761dc4b3429bbe42a35db2e
riyaraa <rinosatyaputra.id@gmail.com>
2021-09-22 21:10:30 +0700

    added environment paytix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -6,9 +7,7 @@ const helmet = require("helmet");
 const compression = require("compression");
 const routerNavigation = require("./routes");
 const bodyParser = require("body-parser");
-
 const app = express();
-
 app.use(morgan("dev"));
 app.use(cors());
 app.options("*", cors());
